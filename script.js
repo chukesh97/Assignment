ad611d362be59727194f8acfb02f3efaf571bf7a
chukesh001 <chukesh97@gmail.com>
2023-12-06 03:56:02 +0530

    Refactor dropdown menu functionality

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-function myFunction() {
-    var x = document.getElementById("dropdown_menu");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
+// JavaScript
+document.querySelector('.toggle-btn').addEventListener('click', function(event) {
+  event.stopPropagation(); // Prevent the document click event from firing
+  const dropdownMenu = document.querySelector('.dropdown_menu');
+  dropdownMenu.classList.toggle('show');
+
+  if (dropdownMenu.classList.contains('show')) {
+      document.body.style.overflow = 'hidden';
+  } else {
+      document.body.style.overflow = 'initial';
   }
+});
+
+document.addEventListener('click', function(event) {
+  const dropdownMenu = document.querySelector('.dropdown_menu');
+
+  if (!dropdownMenu.contains(event.target) && dropdownMenu.classList.contains('show')) {
+      dropdownMenu.classList.remove('show');
+      document.body.style.overflow = 'initial';
+  }
+});
